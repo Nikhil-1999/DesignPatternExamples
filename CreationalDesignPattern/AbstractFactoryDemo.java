8f838236b0b4aab30e09e3bbcb5a9e56266f2dd3
Nikhil Sharma <nikhil.s@appviewx.com>
2024-01-04 14:01:58 +0530

    Added abstract factory design pattern example update

--- CreationalDesignPattern/AbstractFactoryDemo.java
+++ CreationalDesignPattern/AbstractFactoryDemo.java
@@ -2,6 +2,7 @@
  Abstract Factory Pattern says that just define an interface or abstract class for creating families of related (or dependent) objects
  but without specifying their concrete sub-classes.That means Abstract Factory lets a class returns a factory of classes. 
  So, this is the reason that Abstract Factory Pattern is one level higher than the Factory Pattern.
+ Refs - https://www.geeksforgeeks.org/abstract-factory-pattern/
  */
 
 abstract class Car {
